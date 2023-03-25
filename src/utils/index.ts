ac7d3f6ca72d7065e1c91481a6a8e6c88d20973e
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-03-25 20:29:48 +0600

    Export colors and error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 export { ArrayUtils, Person, SortDirection } from "./array";
+export { ColorFormat, formatColor, parseColor } from "./color";
 export { DateFormat, DateUnitValue, DateUtils } from "./date";
+export { CustomError } from "./error";
 export { MathUtils } from "./math";
 export { ObjectUtils } from "./object";
 export { StringUtils } from "./string";
