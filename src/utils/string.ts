55020a30ec65078d74a7e9a59d1c9d2e29a82289
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-03-25 14:10:22 +0600

    Utils for `String`

---
+++ src/utils/string.ts
@@ -0,0 +1,97 @@
+type TrimInput = string;
+type TrimOutput = string;
+
+type UppercaseInput = string;
+type UppercaseOutput = string;
+
+type LowercaseInput = string;
+type LowercaseOutput = string;
+
+type CharCountInput = string;
+type CharCountOutput = number;
+
+type ReplaceInput = [string, string];
+type ReplaceOutput = string;
+
+type SubstringInput = [number, number?];
+type SubstringOutput = string;
+
+type PadInput = [number, string?];
+type PadOutput = string;
+
+export module StringUtils {
+  /**
+   * Trims whitespace from both ends of a string.
+   * @param str The string to trim.
+   * @returns The trimmed string.
+   */
+  export function trim(str: TrimInput): TrimOutput {
+    return str.trim();
+  }
+
+  /**
+   * Converts a string to uppercase.
+   * @param str The string to convert.
+   * @returns The uppercase string.
+   */
+  export function toUpperCase(str: UppercaseInput): UppercaseOutput {
+    return str.toUpperCase();
+  }
+
+  /**
+   * Converts a string to lowercase.
+   * @param str The string to convert.
+   * @returns The lowercase string.
+   */
+  export function toLowerCase(str: LowercaseInput): LowercaseOutput {
+    return str.toLowerCase();
+  }
+
+  /**
+   * Counts the number of characters in a string.
+   * @param str The string to count characters in.
+   * @returns The number of characters in the string.
+   */
+  export function countCharacters(str: CharCountInput): CharCountOutput {
+    return str.length;
+  }
+  /**
+   * Replaces all occurrences of a substring in a string with another substring.
+   * @param str The string to search and replace in.
+   * @param replace The substring to search for and replace.
+   * @param withStr The substring to replace the found substrings with.
+   * @returns The modified string.
+   */
+  export function replace(
+    str: string,
+    replace: ReplaceInput,
+    withStr: string
+  ): ReplaceOutput {
+    return str.split(replace[0]).join(withStr);
+  }
+
+  /**
+   * Extracts a substring from a string.
+   * @param str The string to extract the substring from.
+   * @param start The starting index of the substring.
+   * @param end The ending index of the substring (optional).
+   * @returns The extracted substring.
+   */
+  export function substring(
+    str: string,
+    [start, end]: SubstringInput
+  ): SubstringOutput {
+    return str.substring(start, end);
+  }
+
+  /**
+   * Pads a string with a specified character to a specified length.
+   * @param str The string to pad.
+   * @param length The total length of the padded string.
+   * @param padChar The character to use for padding (optional, defaults to a space).
+   * @returns The padded string.
+   */
+  export function pad(str: string, [length, padChar]: PadInput): PadOutput {
+    return str.padEnd(length, padChar ?? " ");
+  }
+}
