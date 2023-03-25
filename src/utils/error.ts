944fea429584cc2b0b5528596a2cc91ff0c9eb4d
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-03-25 17:36:45 +0600

    Create CustomErrorCreator

---
+++ src/utils/error.ts
@@ -0,0 +1,46 @@
+enum ErrorCode {
+  BadInput = "bad_input",
+  InvalidData = "invalid_data",
+  ServerError = "server_error",
+}
+
+interface ErrorOptions {
+  message: string;
+  code: ErrorCode;
+  status?: number;
+}
+
+class CustomError extends Error {
+  code: ErrorCode;
+  status: number;
+
+  constructor({ message, code, status = 500 }: ErrorOptions) {
+    super(message);
+    this.name = "CustomError";
+    this.code = code;
+    this.status = status;
+  }
+}
+
+class ECreatorError extends CustomError {
+  constructor(message: string) {
+    super({ message, code: ErrorCode.ServerError });
+    this.name = "ECreatorError";
+  }
+}
+
+class ECreatorTypeError extends CustomError {
+  constructor(message: string) {
+    super({ message, code: ErrorCode.BadInput });
+    this.name = "ECreatorTypeError";
+  }
+}
+
+class ECreatorSyntaxError extends CustomError {
+  constructor(message: string) {
+    super({ message, code: ErrorCode.InvalidData });
+    this.name = "ECreatorSyntaxError";
+  }
+}
+
+export { CustomError };
